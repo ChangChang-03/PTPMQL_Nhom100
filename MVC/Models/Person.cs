a9c54d9dd0f8e565696231904665ce77695e6f49
ChangChang-03 <trangha0303@gmail.com>
2025-08-27 22:57:57 +0700

    Models/Person.cs

---
+++ MVC/Models/Person.cs
@@ -0,0 +1,8 @@
+namespace MVC.Models.Entities
+{
+    public class Person
+    {
+        public string StudentID { get; set; } = default!;
+        public string FullName { get; set; } = default!;
+    }
+}
