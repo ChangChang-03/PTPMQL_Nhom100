7793ba9138713da5f3aa52300ff344d5692369c1
ChangChang-03 <trangha0303@gmail.com>
2025-12-04 15:39:23 +0700

    TuyChinh, MotSoCauHinhTrongIdentity

--- MVC/Data/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace MVC.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public string MaSinhVien { get; set; } // mã sinh tự động
-    }
-}
--- MVC/Program.cs
+++ MVC/Program.cs
@@ -1,21 +1,58 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using VicemMVCIdentity.MVC.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Connection string cho DbContext của MVC.Data
-var mvcConnectionString = builder.Configuration.GetConnectionString("DefaultConnection"); 
+var mvcConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+// DB MVC
 builder.Services.AddDbContext<MVC.Data.ApplicationDbContext>(options =>
     options.UseSqlServer(mvcConnectionString));
 
-// Identity DbContext
-builder.Services.AddDbContext<VicemMVCIdentity.MVC.Areas.Identity.Data.ApplicationDbContext>(options =>
+// DB Identity
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(mvcConnectionString));
 
-builder.Services.AddDefaultIdentity<VicemMVCIdentity.MVC.Areas.Identity.Data.ApplicationUser>()
-    .AddEntityFrameworkStores<VicemMVCIdentity.MVC.Areas.Identity.Data.ApplicationDbContext>();
+// Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    // Lockout
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
 
+    // Password
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+
+    // Login
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+    options.SignIn.RequireConfirmedAccount = false;
+
+    // User
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddAuthorization();
+
+// Cookie
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.SlidingExpiration = true;
+});
 
 var app = builder.Build();
 
@@ -30,13 +67,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication(); // thêm
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapRazorPages(); // cần cho Identity
+app.MapRazorPages();
 
 app.Run();
